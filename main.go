ff15e2e9b77f1bcd3840cac4ea8632e3d686df5a
hzzhulilei <hzzhulilei@corp.netease.com>
2017-01-09 16:39:39 +0800

    add proxy_pass find

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ var (
 	//file_name   = flag.String("F", "", "filename_name")
 	path_name     = flag.String("P", "", "path_name")
 	upstream_name = flag.String("U", "", "upstream_name")
+	with_proxy    = flag.Bool("A", false, "false")
+	proxy_array   []string
 )
 
 func main() {
@@ -37,7 +39,7 @@ func main() {
 	}
 
 	for i := 0; i < len(fileList)-1; i++ {
-		if asFile(fileList[i]) {
+		if asFile(fileList[i], *server_name, *upstream_name) {
 			fmt.Printf("\n %c[1;40;32m%s%c[0m\n\n", 0x1B, fileList[i], 0x1B)
 			//fmt.Printf(fileList[i] + "\r\n")
 		}
@@ -45,12 +47,11 @@ func main() {
 
 }
 
-func asFile(file_name string) bool {
+func asFile(file_name, server_name, upstream_name string) bool {
 	result := false
 	records = []string{""}
 	getStrings(file_name)
-	matchIndexSlice := records.matchName(*server_name, *upstream_name)
-	//fmt.Println(matchIndexSlice)
+	matchIndexSlice := records.matchName(server_name, upstream_name)
 
 	for _, matchIndex := range matchIndexSlice {
 		startIndex := records.findStart(matchIndex)
@@ -72,7 +73,20 @@ func asFile(file_name string) bool {
 		case 0:
 			for index := startIndex - 1; index < endIndex; index++ {
 				fmt.Printf(records[index])
+				proxy := getProxyPass(records[index])
+				if proxy != "" {
+					proxy_array = append(proxy_array, proxy)
+				}
 				result = true
+			}
+
+			//fmt.Println(proxy_array)
+			//fmt.Println("over")
+			for _, value := range proxy_array {
+				//fmt.Println("%d: %s", i, value)
+				if *with_proxy != false {
+					listProxyPass(file_name, value)
+				}
 			}
 
 		case 1:
@@ -92,6 +106,12 @@ func CheckErr(err error) {
 	if nil != err {
 		panic(err)
 	}
+}
+
+func listProxyPass(file_name, upstream string) {
+	find = 1
+	asFile(file_name, "", upstream)
+
 }
 
 func getStrings(filename string) nginxslice {
@@ -139,6 +159,17 @@ func (records nginxslice) matchName(server_name string, upstream_name string) (m
 
 	return matchIndexSlice
 
+}
+
+func getProxyPass(line string) (proxy string) {
+	if strings.Contains(line, "proxy_pass") {
+		proxyArray := strings.Split(line, "//")
+		proxy = strings.Replace(proxyArray[len(proxyArray)-1], ";", "", -1)
+		proxy = strings.Replace(proxy, "\n", "", -1)
+		//fmt.Println("proxyArray is", proxy)
+		return proxy
+	}
+	return
 }
 
 func (records nginxslice) findStart(matchIndex int) (startIndex int) {
